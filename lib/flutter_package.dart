bcd950d43a7d84032d41327de5ec060c2911890d
gongxiaojun <1258624735@qq.com>
2018-06-28 11:07:57 +0800

    Initial commit

---
+++ lib/flutter_package.dart
@@ -0,0 +1,50 @@
+library flutter_package;
+
+/**
+ * dart 基础学习
+ */
+void main() {
+  test1();
+}
+
+/**
+ * dart 基础常量学习
+ */
+void test1() {
+  print("nihao");
+  var str1 = "123";
+  String str2 = "nihao";
+  //三引号 可以多行编写
+  var str3 = """ iajap
+  nihao
+  """;
+  //断言 不符合立即终止执行
+  assert(str1 == "123");
+  //字符串输出
+  print("name： $str1");
+  print("name" + "haha");
+  var text1 = 1;
+  print(text1);
+
+  //编译时常量
+  const time = 1;
+  //变量型常量
+  final time1 = new DateTime.now();
+}
+
+/**
+ * 函数学习 方法学习
+ */
+void test2() {
+  var myString = demo1();
+  
+}
+
+String demo1() {
+  return "nihao";
+}
+
+/**
+ * =>  匿名函数的写法  只有一个表达式的 简单写法
+ */
+String demo2() => "nihao";

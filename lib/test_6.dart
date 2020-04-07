17cb80ad967ac7c30e5f6f8258b4cd2085e1fb02
gxj <1258624735@qq.com>
2020-04-07 17:24:21 +0800

    初始化提交

--- lib/test_6.dart
+++ lib/test_6.dart
@@ -10,25 +10,90 @@ void main() {
 //  print("2222");
 //  test.getTest1();
 
-  _timeFunc("2020-04-06 16:20:20.0");
+//  _timeFunc("2020-04-06 16:20:20.0");
+//  String time = handleDate("2020-04-07 2:20:20.0");
+//  print(time);
+
+// 测试定时取值
+  List<String> money = ["101", "102", "103", "104"];
+  List<String> time = ["1分钟", "2分钟", "3分钟", "4分钟"];
+  int _scrollIndex = 0; //当前收益轮播到的 条目
+  String _preIncome = "0.00"; //正在显示收益
+  String _nextIncome = "0.00"; //将要显示的下一个收益
+  String _preTime = ""; //当前显示的收益时间
+  Timer _timer = Timer.periodic(Duration(seconds: 2), (timer) {
+    print("_scrollIndex=" + _scrollIndex.toString());
+    if (money.length == 1) {
+      //赋值显示的收益
+      _preIncome = money[_scrollIndex];
+      _nextIncome = money[_scrollIndex];
+      _preTime = time[_scrollIndex];
+      _scrollIndex++;
+    } else if (money.length > 1 && _scrollIndex < money.length - 1) {
+      //赋值显示的收益
+      _preIncome = money[_scrollIndex];
+      _nextIncome = money[_scrollIndex + 1];
+      _preTime = time[_scrollIndex];
+      _scrollIndex++;
+    } else if (money.length > 1 && _scrollIndex == money.length - 1) {
+      //赋值显示的收益
+      _preIncome = money[_scrollIndex];
+      _nextIncome = money[0];
+      _preTime = time[_scrollIndex];
+      _scrollIndex++;
+    } else {
+      _preIncome = "0.00";
+      _nextIncome = "0.00";
+      _scrollIndex = 0;
+    }
+    print("_preIncome=" + _preIncome + "|_nextIncome=" + _nextIncome);
+    print("_preTime=" + _preTime);
+    //滑动最后一个重置index
+    if (_scrollIndex == money.length) {
+      _scrollIndex = 0;
+    }
+  });
 }
 
-void _timeFunc(time) {
-  DateTime _diffDate = DateTime.tryParse(time);
-  var _newDate = DateTime.now();
-  var _surplus = _diffDate.difference(_newDate);
-  int _day = (_surplus.inSeconds ~/ 3600) ~/ 24;
-  int hour = (_surplus.inSeconds ~/ 3600) % 24;
-  int minute = _surplus.inSeconds % 3600 ~/ 60;
-  int second = _surplus.inSeconds % 60;
-  // formatTime(hour) + ":" + formatTime(minute) + ":" + formatTime(second);
-  var _day1 = _day.toString() ?? '0';
-  var _hour1 = hour.toString() ?? '0';
-  var _minute1 = minute.toString() ?? '0';
-  var _text = _newDate.hour.toString()+':' + _newDate.minute.toString();
-  print(_day1.toString() + "|" + _hour1.toString() + "|" + _minute1.toString() + "");
-  print(_surplus.inDays.toString() + "|" + _surplus.inHours.toString() + "|" + _surplus.inMinutes.toString() + "");
-  print(_text.toString());
+//时间差处理
+String handleDate(String oldTime) {
+  String nowTime =
+      new DateTime.now().toString().split('.')[0].replaceAll('-', '/');
+  int nowYear = int.parse(nowTime.split(" ")[0].split('/')[0]);
+  int nowMonth = int.parse(nowTime.split(" ")[0].split('/')[1]);
+  int nowDay = int.parse(nowTime.split(" ")[0].split('/')[2]);
+  int nowHour = int.parse(nowTime.split(" ")[1].split(':')[0]);
+  int nowMin = int.parse(nowTime.split(" ")[1].split(':')[1]);
+
+  int oldYear = int.parse(oldTime.split(" ")[0].split('-')[0]);
+  int oldMonth = int.parse(oldTime.split(" ")[0].split('-')[1]);
+  int oldDay = int.parse(oldTime.split(" ")[0].split("-")[2]);
+  int oldHour = int.parse(oldTime.split(" ")[1].split(':')[0]);
+  int oldMin = int.parse(oldTime.split(" ")[1].split(':')[1]);
+
+  var now = new DateTime(nowYear, nowMonth, nowDay, nowHour, nowMin);
+  var old = new DateTime(oldYear, oldMonth, oldDay, oldHour, oldMin);
+  var difference = now.difference(old);
+  if (difference.inDays > 1) {
+    if (difference.inHours - (difference.inDays * 24) - nowHour >= 24) {
+      return (difference.inDays + 1).toString() + '天前';
+    } else {
+      return (difference.inDays).toString() + '天前';
+    }
+  } else if (difference.inDays == 1) {
+    if (difference.inHours - nowHour >= 24) {
+      return 2.toString() + '天前';
+    } else {
+      return '昨天'.toString();
+    }
+  } else if (difference.inHours >= 1 && difference.inHours < 24) {
+    return (difference.inHours).toString() + '小时前';
+  } else if (difference.inMinutes > 5 && difference.inMinutes < 60) {
+    return (difference.inMinutes).toString() + '分钟前';
+  } else if (difference.inMinutes <= 5) {
+    return '刚刚';
+  }
+  return "";
 }
 
 ///测试 await== 等待耗时操作   Future 代表修饰此方法是耗时方法
